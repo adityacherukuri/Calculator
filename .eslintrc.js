bd6f625a7147c3ace5894c810479f54a45f3b778
lcherukuri <lokesh.cherukuri@am.sony.com>
2020-07-17 16:29:08 -0700

    add operation

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,16 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2020: true,
-    node: true,
-  },
-  extends: 'airbnb-base',
-  parserOptions: {
-    ecmaVersion: 11,
-    sourceType: 'module',
-  },
-  rules: {
-
-  },
+    env: {
+        browser: true,
+        es2020: true,
+        node: true,
+        jest: true,
+    },
+    extends: 'airbnb-base',
+    parserOptions: {
+        ecmaVersion: 11,
+        sourceType: 'module',
+    },
+    rules: {
+        indent: ['error', 4],
+    },
 };
